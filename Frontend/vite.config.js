c3587e606d4d8b7e634120ae65a2701c5ac4ccfe
7085spaul <Im68018801@gmail.com>
2025-02-19 12:47:38 +0530

    Set Node.js version to 18.x

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,16 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build", // Change default 'dist' folder to 'build'
-  }, // <-- Missing comma was added here
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-      },
-    },
-  },
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
 });
-
